8b99ce5d9515cf19e0260605e06da74e2fcfc5e7
MarylineGames <mariavidajoia@gmail.com>
2021-12-14 14:28:40 -0300

    initial commit

---
+++ Spes/Assets/Scripts/Movimentacao.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D playerRB;
+    public Animator animator;
+
+    Vector2 movimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movimento.x);
+        animator.SetFloat("Vertical", movimento.y);
+        animator.SetFloat("Velocidade", movimento.sqrMagnitude);
+
+        if (movimento != Vector2.zero)
+            {
+            animator.SetFloat("horizontalidle", movimento.x);
+            animator.SetFloat("verticalidle", movimento.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRB.MovePosition(playerRB.position + movimento * speed * Time.fixedDeltaTime);
+    }
+}
